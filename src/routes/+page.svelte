ea5034363505fd22c9388b5002734681016ac0e4
harshpx <harsh.rzf@gmail.com>
2025-09-01 00:56:55 +0530

    tried no-snapping in mobile devices

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,15 +10,15 @@
 </div>
 <div
 	id="homepage"
-	class="flex h-[calc(100vh-60px)] w-full snap-y snap-proximity flex-col gap-2 overflow-y-auto scroll-smooth px-2 pb-2"
+	class="flex h-[calc(100vh-60px)] w-full flex-col gap-2 overflow-y-auto px-2 pb-2 sm:snap-y sm:snap-proximity sm:scroll-smooth"
 >
-	<div id="home" class="flex h-full w-full shrink-0 snap-center items-center justify-center">
+	<div id="home" class="flex h-full w-full shrink-0 items-center justify-center sm:snap-center">
 		<Home />
 	</div>
-	<div id="about" class="min-h-full w-full shrink-0 snap-center">
+	<div id="about" class="min-h-full w-full shrink-0 sm:snap-center">
 		<About />
 	</div>
-	<div id="projects" class="min-h-full w-full shrink-0 snap-center">
+	<div id="projects" class="min-h-full w-full shrink-0 sm:snap-center">
 		<Projects />
 	</div>
 </div>
